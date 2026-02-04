85f12b126964cfee68983e2ac92b98b7256a3d56
raibaebe <kasimkizyraikhan@gmail.com>
2026-02-04 17:22:05 +0500

    Auto-create admin user on deploy

--- build.sh
+++ build.sh
@@ -10,3 +10,14 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@cloudmtrx.com', 'Admin123!')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+EOF
